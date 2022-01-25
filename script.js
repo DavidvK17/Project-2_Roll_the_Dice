107fca03a81031a550fc5adf0e6b029fe6b22bee
DavidvK17 <david.sing7@gmail.com>
2022-01-25 22:18:29 +0100

    Generate Number and display dice

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const rollDice = document.getElementById('roll--dice');
+let img = document.getElementById('img');
+
+console.log(img);
+
+// generate randome dice roll
+function diceRoll() {
+  const dice = Math.trunc(Math.random() * 6 + 1);
+  console.log(dice);
+
+  // display generated dice
+  img.src = `/img/dice-${dice}.png`;
+  img.style.display = 'block';
+}
+
+// generate randome dice roll on rollDice click
+rollDice.addEventListener('click', diceRoll);
