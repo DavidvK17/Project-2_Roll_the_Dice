ccf14f33a37754bb703d9f04f5565cd77d46709f
DavidvK17 <david.sing7@gmail.com>
2022-01-25 22:32:10 +0100

    Update Score and CurScore Player0

--- script.js
+++ script.js
@@ -2,18 +2,34 @@
 
 const rollDice = document.getElementById('roll--dice');
 let img = document.getElementById('img');
+let score0 = 0;
+let displayScore0 = document.getElementById('score--0');
 
-console.log(img);
+const holdScore = document.getElementById('hold');
+let curScore0 = 0;
+let displayCurScore0 = document.getElementById('current--score--0');
 
 // generate randome dice roll
 function diceRoll() {
   const dice = Math.trunc(Math.random() * 6 + 1);
-  console.log(dice);
 
   // display generated dice
   img.src = `/img/dice-${dice}.png`;
   img.style.display = 'block';
+
+  // update player0 score
+  score0 += dice;
+  displayScore0.textContent = `${score0}`;
+}
+
+function updateCurScore() {
+  curScore0 += score0;
+  displayCurScore0.textContent = `${curScore0}`;
+  console.log(curScore0);
 }
 
 // generate randome dice roll on rollDice click
 rollDice.addEventListener('click', diceRoll);
+
+// update player0 current score on hold
+holdScore.addEventListener('click', updateCurScore);
