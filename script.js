0e5220c9aa0b927608cb60f883b6d9e47c494f5a
DavidvK17 <david.sing7@gmail.com>
2022-01-26 22:28:15 +0100

    fixing bug on rolling 1 empty score

--- script.js
+++ script.js
@@ -16,60 +16,57 @@ let curPlayer = document.getElementsByClassName('player--container--0')[0];
 const playing = curPlayer.classList.contains('is--active');
 
 let scores = [0, 0];
+let curScore = 0;
 
-if (playing) {
-  // generate randome dice roll
-  function diceRoll() {
-    const dice = Math.trunc(Math.random() * 6 + 1);
-    // display generated dice
-    img.src = `/img/dice-${dice}.png`;
-    img.style.display = 'block';
+// generate randome dice roll
+function diceRoll() {
+  const dice = Math.trunc(Math.random() * 6 + 1);
+  // display generated dice
+  img.src = `/img/dice-${dice}.png`;
+  img.style.display = 'block';
 
-    if (playing && dice !== 1) {
-      // update player0 score
-      score += dice;
-      displayScore.textContent = `${score}`;
-    } else {
-      switchPlayer();
-    }
-  }
-
-  function updateCurScore() {
-    let curScore = scores[activePlayer];
-    curScore += score;
-    console.log(scores);
-    console.log(scores[activePlayer]);
-    console.log(curScore);
-
-    displayCurScore.textContent = `${curScore}`;
-    score = 0;
-    displayScore.textContent = 0;
-    img.style.display = 'none';
+  if (playing && dice !== 1) {
+    // update player0 score
+    score += dice;
+    displayScore.textContent = `${score}`;
+  } else {
     switchPlayer();
   }
-
-  function switchPlayer() {
-    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
-    displayScore = document.getElementById(`score--${activePlayer}`);
-    displayCurScore = document.getElementById(
-      `current--score--${activePlayer}`
-    );
-
-    if (curPlayer.classList.contains('is--active')) {
-      curPlayer.classList.remove('is--active');
-      curPlayer = document.getElementsByClassName(
-        `player--container--${activePlayer}`
-      )[0];
-      curPlayer.classList.add('is--active');
-    }
-
-    score = 0;
-    displayScore.textContent = `${score}`;
-  }
-
-  // generate randome dice roll on rollDice click
-  rollDice.addEventListener('click', diceRoll);
-
-  // update player0 current score on hold
-  holdScore.addEventListener('click', updateCurScore);
 }
+
+function updateCurScore() {
+  curScore = scores[activePlayer];
+  curScore = score;
+
+  console.log(scores);
+  console.log(scores[activePlayer]);
+  console.log(curScore);
+
+  displayCurScore.textContent = `${curScore}`;
+  score = 0;
+  displayScore.textContent = 0;
+  img.style.display = 'none';
+  switchPlayer();
+}
+
+function switchPlayer() {
+  score = 0;
+  displayScore.textContent = `${score}`;
+  activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+  displayScore = document.getElementById(`score--${activePlayer}`);
+  displayCurScore = document.getElementById(`current--score--${activePlayer}`);
+
+  if (curPlayer.classList.contains('is--active')) {
+    curPlayer.classList.remove('is--active');
+    curPlayer = document.getElementsByClassName(
+      `player--container--${activePlayer}`
+    )[0];
+    curPlayer.classList.add('is--active');
+  }
+}
+
+// generate randome dice roll on rollDice click
+rollDice.addEventListener('click', diceRoll);
+
+// update player0 current score on hold
+holdScore.addEventListener('click', updateCurScore);
